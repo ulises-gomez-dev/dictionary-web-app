e500b8eb44118838eab98a76e5a1e17f9f03dbb9
Ulises <ulises.gomez.dev@proton.me>
2025-07-06 23:47:10 -0700

    fixed Navbar layout issues temporarily.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,15 @@
 import ToggleSwitch from "./ToggleSwitch";
-import { useState } from "react";
 import "../styles/Navbar.css";
 
 function Navbar({ className }) {
   const font = "sans-serif";
-
-  const [showSelect, setShowSelect] = useState(false);
-
-  const toggleSelect = () => {
-    setShowSelect(!showSelect);
-  };
 
   return (
     <div className={className}>
       <img src="./src/assets/images/logo.svg" alt="" />
       <div className="modes">
         <button className={`font ${font}`}>Sans Serif</button>
+        <span className="line"></span>
         <div className="theme">
           <ToggleSwitch className="switch" />
           <img src="./src/assets/images/icon-moon.svg" alt="" />
